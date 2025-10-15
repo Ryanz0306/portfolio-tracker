d09181b7c7ca2099d9f8deea1546e9a55b0df660
Rz <ryanzhaowork@gmail.com>
2025-10-15 22:09:10 +0800

    First Iteration (Ticker & API)

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,10 @@
+from flask import Flask, jsonify, request, render_template 
+import json
+import os 
+from datetime import datetime
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
